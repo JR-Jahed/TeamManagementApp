26b9bebaa13d01a5a657a93b9f2b2a9052a58934
JR Jahed <jrjahed100@gmail.com>
2023-05-14 11:05:14 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    ),
+  );
+}
